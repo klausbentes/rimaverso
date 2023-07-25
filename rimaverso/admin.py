98d3cc2634c643cde3c3374e055948591c43094e
Klaus Bentes <klausbentes@gmail.com>
2023-07-25 10:58:22 -0300

    Personalização do Django Admin.

--- rimaverso/admin.py
+++ rimaverso/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rimaverso.models import Dicionario
 
 # Register your models here.
+
+class ListandoPalavras(admin.ModelAdmin):
+    list_display = ('palavra', 'pronuncia', 'rima')
+    search_fields = ('palavra',)
+    list_display_links = ('palavra', 'pronuncia', 'rima')
+    list_filter = ('silabas',)
+    ordering = ('id',)
+
+admin.site.register(Dicionario, ListandoPalavras)
